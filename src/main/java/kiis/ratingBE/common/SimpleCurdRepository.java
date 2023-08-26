e9ec32be61b0a3938c384962c484b9a73bba7300
vuhungson <vuhungson158@gmail.com>
2023-08-26 09:46:30 +0900

    config swagger

--- src/main/java/kiis/ratingBE/common/SimpleCurdRepository.java
+++ src/main/java/kiis/ratingBE/common/SimpleCurdRepository.java
@@ -5,14 +5,24 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface SimpleCurdRepository<T extends BaseEntity> extends JpaRepository<T, Long> {
 
+    @Query(nativeQuery = true, value = """
+            SELECT 1
+            """)
     List<T> findAllByIsDeletedIsFalse();
 
+    @Query(nativeQuery = true, value = """
+            SELECT 1
+            """)
     Page<T> findAllByIsDeletedIsFalse(Pageable pageable);
 
+    @Query(nativeQuery = true, value = """
+            SELECT 1
+            """)
     <S extends T> @NotNull Page<S> findAllByIsDeletedIsFalse(@NotNull Example<S> example, @NotNull Pageable pageable);
 }
--- src/main/java/kiis/ratingBE/helper/SwaggerConfig.java
+++ src/main/java/kiis/ratingBE/helper/SwaggerConfig.java
@@ -1,48 +1,28 @@
 package kiis.ratingBE.helper;
 
-import kiis.ratingBE.RatingBeApplication;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.service.SecurityScheme;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.http.HttpHeaders;
 
-import java.util.Collections;
-import java.util.List;
-
-@EnableSwagger2
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public Docket api() {
-        // securitySchemeList
-        final List<SecurityScheme> securitySchemeList = Collections.singletonList(
-                new ApiKey("JWT Token", Constant.TOKEN_HEADER, "header")
-        );
-
-        // securityContextList
-        final List<SecurityReference> securityReferenceList = Collections.singletonList(
-                new SecurityReference("JWT Token", new AuthorizationScope[]{
-                        new AuthorizationScope("global", "accessEverything")
-                })
-        );
-        final List<SecurityContext> securityContextList = Collections.singletonList(
-                SecurityContext.builder().securityReferences(securityReferenceList).build()
-        );
-
-        return new Docket(DocumentationType.SWAGGER_2)
-                .securitySchemes(securitySchemeList)
-                .securityContexts(securityContextList)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage(RatingBeApplication.class.getPackage().getName()))
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI customOpenAPI() {
+        final String MapKey = "Token";
+        return new OpenAPI()
+                .components(
+                        new Components().addSecuritySchemes(
+                                MapKey,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name(HttpHeaders.AUTHORIZATION)
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList(MapKey));
     }
 }
