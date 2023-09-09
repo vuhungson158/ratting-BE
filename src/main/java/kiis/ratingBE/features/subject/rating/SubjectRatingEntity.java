456a29599f2846aa91f299fbc27be7047a835a0b
vuhungson <vuhungson158@gmail.com>
2023-09-09 21:34:56 +0900

    copy User Auth from 2.7.6

---
+++ src/main/java/kiis/ratingBE/features/subject/rating/SubjectRatingEntity.java
@@ -0,0 +1,10 @@
+package kiis.ratingBE.features.subject.rating;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import kiis.ratingBE.common.BaseEntity;
+
+@Entity
+@Table(name = "subject-rating")
+public class SubjectRatingEntity extends BaseEntity {
+}
