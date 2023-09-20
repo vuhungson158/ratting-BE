1aa8d65665770b080cc9be364707d62af0d38aa0
vuhungson <vuhungson158@gmail.com>
2023-09-20 14:34:24 +0900

    static

--- src/main/java/kiis/ratingBE/helper/Util.java
+++ src/main/java/kiis/ratingBE/helper/Util.java
@@ -5,17 +5,23 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class Util {
-    private static final ObjectMapper mapper = new ObjectMapper();
+    final static ObjectMapper mapper = new ObjectMapper();
+    final static Logger logger = LoggerFactory.getLogger(Util.class);
 
-    public static <T> @Nullable T mapping(Object object, Class<T> type) {
+    static {
         mapper.registerModule(new JavaTimeModule());
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    public static <T> @Nullable T mapping(Object object, Class<T> type) {
         try {
             return mapper.readValue(mapper.writeValueAsString(object), type);
         } catch (final JsonProcessingException e) {
-            e.printStackTrace();
+            logger.error(e.getMessage(), e);
             return null;
         }
     }
