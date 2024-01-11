7bb932f7a437e3b3deac1a9142a8e0891a32adc5
vuhungson <vuhungson158@gmail.com>
2024-01-11 15:56:56 +0900

    add Bean AuthenticationManager to disable default spring security default password

--- src/main/java/kiis/ratingBE/security/SecurityConfig.java
+++ src/main/java/kiis/ratingBE/security/SecurityConfig.java
@@ -9,7 +9,9 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
@@ -37,6 +39,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests(endpointAuthorizeConfig)
                 .exceptionHandling(exceptionHandlingCustomizer());
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Contract(pure = true)
