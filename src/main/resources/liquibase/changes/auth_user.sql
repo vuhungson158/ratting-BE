39f19de4def0e0aeec21c90496582d5eb2bc5d6e
vuhungson <vuhungson158@gmail.com>
2024-01-11 12:45:47 +0900

    auth_user.sql changeset test ok

--- src/main/resources/liquibase/changes/auth_user.sql
+++ src/main/resources/liquibase/changes/auth_user.sql
@@ -1,3 +1,5 @@
+--liquibase formatted sql
+--changeset SonVH25:create-table-auth_user
 create table auth_user
 (
     id           bigserial primary key,
@@ -6,11 +8,19 @@ create table auth_user
     created_at   timestamp(6) with time zone,
     updated_at   timestamp(6) with time zone,
     created_by   varchar(255),
+    updated_by   varchar(255),
 
     display_name varchar(255),
     email        varchar(255) unique,
     gender       varchar(255) not null,
     password     varchar(255),
-    role         varchar(255) not null,
-    updated_by   varchar(255)
+    role         varchar(255) not null
 );
+
+--changeset SonVH25:alter-table-auth_user-add-column-test
+alter table auth_user
+    add column test int unique not null default 0;
+
+--changeset SonVH25:alter-table-auth_user-drop-column-test
+alter table auth_user
+    drop column test;
